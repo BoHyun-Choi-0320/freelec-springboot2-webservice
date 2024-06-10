c4b8472ea8d04ea13997eeb69faabe2546535b2d
choibohyun <320fleur@gmail.com>
2024-06-11 00:42:59 +0900

    ~ Chapter.03 등록/수정/조회 API 만들기

--- src/main/java/com/banbi/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/banbi/book/springboot/service/posts/PostsService.java
@@ -1,6 +1,9 @@
 package com.banbi.book.springboot.service.posts;
 
+import com.banbi.book.springboot.domain.posts.Posts;
 import com.banbi.book.springboot.domain.posts.PostsRepository;
+import com.banbi.book.springboot.web.dto.PostsResponseDto;
+import com.banbi.book.springboot.web.dto.PostsUpdateRequestDto;
 import com.banbi.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +17,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id ="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
